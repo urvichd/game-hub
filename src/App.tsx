f21974cad58f482edc04c53be2584dd64d6c533d
urvichd@gmail.com <urvi.sharma@netsolutions.com>
2023-05-24 00:08:24 +1200

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
   return (
     <div>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav"  "aside main"`,
+        }}
+        //gridTemplateRows={"50px 1fr 30px"}
+        //gridTemplateColumns={"150px 1fr"}
+        h="100px"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem bg="orange.200" area={"nav"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="yellow.300" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="pink.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 }
